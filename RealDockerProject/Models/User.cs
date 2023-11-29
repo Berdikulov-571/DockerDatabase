72fbb479bd959fe9d3c4158fa0b70acfd49aaa5a
berdikulov-571 <mygithub571@gmail.com>
2023-11-29 23:47:38 +0500

    User Model

--- RealDockerProject/Models/User.cs
+++ RealDockerProject/Models/User.cs
@@ -10,7 +10,7 @@ namespace RealDockerProject.Models
         [JsonIgnore]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
     }
 }
